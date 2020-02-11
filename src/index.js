f16a25211c43a60173ad78aa6c0857f684ce5a19
KruBob <res.19@yandex.ru>
2020-02-11 17:21:39 +0300

    Добавляет валидацию в компонент main

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
 const Settings = {
-  PLACES_COUNT: 312
+  PLACES_COUNT: 312,
 };
+const placesNames = [`Beautiful & luxurious apartment at great location`, `Wood and stone place`];
 
 ReactDOM.render(
     <App
       placesCount={Settings.PLACES_COUNT}
+      placesNames={placesNames}
     />,
     document.querySelector(`#root`)
 );
