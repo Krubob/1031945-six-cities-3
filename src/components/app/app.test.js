2dacaeb74f123f4cd9dc0576c2b23ed6ec487f51
KruBob <res.19@yandex.ru>
2020-02-12 20:59:08 +0300

    Установит и настроит jest, добавит снэпшот тесты

---
+++ src/components/app/app.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "./app.jsx";
+
+const Settings = {
+  PLACES_COUNT: 312
+};
+const placesNames = [`Beautiful & luxurious apartment at great location`, `Wood and stone place`];
+
+it(`Render App`, () => {
+  const tree = renderer
+    .create(<App
+      placesCount={Settings.PLACES_COUNT}
+      placesNames={placesNames}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
---
+++ src/components/main/main.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Main from "./main.jsx";
+
+const Settings = {
+  PLACES_COUNT: 312
+};
+const placesNames = [`Beautiful & luxurious apartment at great location`, `Wood and stone place`];
+
+it(`Should Main render correctly`, () => {
+  const tree = renderer
+    .create(<Main
+      placesCount={Settings.PLACES_COUNT}
+      placesNames={placesNames}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
